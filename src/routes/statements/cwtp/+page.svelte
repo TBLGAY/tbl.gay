c288dc05a88508b3e650fcc2fe4ed72a366914cc
Emily Signet <emily@cat-girl.gay>
2023-08-22 11:44:37 -0400

    CWTP statement

---
+++ src/routes/statements/cwtp/+page.svelte
@@ -0,0 +1,151 @@
+<svelte:head>
+  <title>tblgay | statement on CWTP's closure</title>
+</svelte:head>
+
+<main>
+  <h1 style="margin-bottom: 0.2em">
+    <strong>Statement on CWTP’s closure</strong>
+  </h1>
+  <p style="text-align:center; margin-top: 0em; margin-bottom:2em;">
+    <em>August 22, 2023</em> <br /><br />
+    <i
+      ><a href="/pdfs/cwtp.pdf" target="_blank" rel="noopener noreferrer"
+        >This document is also available in PDF form.</a
+      ></i
+    >
+  </p>
+  <p>
+    <strong>TBLGAY’s stance</strong>
+  </p>
+  <p>
+    As you may already know, the Centre for Women and Trans People at York
+    University closed its doors on August 18th, 2023. We highly recommend you
+    read the Centre’s official statement on the matter before this one.
+  </p>
+  <p>
+    The news has not been easy on us — both as frequent organizational partners
+    to the Centre, and also as members of the same community on campus; a
+    community that both needs and deserves more services, not less.
+  </p>
+  <p>
+    However, it is with this same conviction that we support the actions taken
+    by CWTP’s Board of Directors in closing the organization. While our
+    community desperately needs services, they <em>must</em> be equitable, sustainable,
+    cooperative and safe in nature. It is clear that, as it stood, the Centre was
+    structurally unable to fulfill these needs, despite the Board’s every effort.
+  </p>
+  <p>
+    Thus, while we grieve the loss of a cornerstone of the community, we also
+    strongly believe that this loss is better than the continuation of a
+    dysfunctional and harmful structure, hard as it is to face.
+  </p>
+  <p>
+    <strong>What we’re doing in the wake</strong>
+  </p>
+  <p>
+    As a Community Service Group, or CSG, TBLGAY’s mission is to provide
+    services to the queer community on campus. In the wake of the loss of CWTP’s
+    own services to our community, we’re redoubling our efforts to live up to
+    that mission.
+  </p>
+  <p>
+    Sadly, we cannot provide services on the same scale as the Centre could’ve.
+    We’re a smaller organization, with less personnel and an order of magnitude
+    smaller budget. But what we can do — be a safe harbour to our community — we
+    will. No matter what. And despite our limits, we’ll try our best to
+    coordinate with our ally organizations to not let services like food and
+    financial aid fall through the cracks.
+  </p>
+  <p style="text-align: center; margin-top: 1em; margin-bottom: 2em;">
+    <strong>We keep us safe.</strong>
+  </p>
+  <p>
+    Kore M.
+    <br /><i />Coordinator of TBLGAY.
+  </p>
+
+  <footer>
+    <address>
+      Trans, Bisexual, Lesbian, Gay Asexual at York <br />
+      York University, Keele Campus <br />
+      First Student Centre, 449A<br />
+      <a href="mailto:tblgayinternal@yfs.ca">tblgayinternal@yfs.ca</a>
+    </address>
+  </footer>
+</main>
+
+<style>
+  * {
+    font-family: "Roboto Serif", "sans-serif";
+  }
+
+  main {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-items: center;
+    width: 85%;
+    margin: auto;
+  }
+
+  h1 {
+    font-size: 2rem;
+  }
+
+  p,
+  footer,
+  ol {
+    width: 100%;
+    text-align: justify;
+    font-size: 1rem;
+    margin-top: 0.2em;
+  }
+
+  figure {
+    margin-top: 2em;
+    margin-bottom: 2em;
+    font-size: 1rem;
+  }
+
+  li {
+    margin-bottom: 0.3em;
+    font-size: 1rem;
+  }
+
+  @media only screen and (min-device-width: 320px) and (max-device-width: 480px) and (-webkit-min-device-pixel-ratio: 2) and (orientation: portrait) {
+  }
+
+  @media (min-width: 320px) and (orientation: portrait) {
+    :global(html) {
+      font-size: 32px;
+    }
+  }
+
+  @media (min-width: 480px) and (orientation: portrait) {
+    :global(html) {
+      font-size: 32px;
+    }
+  }
+
+  @media (min-width: 720px) and (orientation: landscape) {
+    :global(html) {
+      font-size: 18px;
+    }
+
+    main {
+      width: 70%;
+    }
+  }
+
+  a {
+    color: green;
+  }
+
+  .row {
+    margin-top: 1em;
+    display: flex;
+    flex-flow: row wrap;
+    justify-content: space-evenly;
+    width: 100%;
+  }
+</style>
