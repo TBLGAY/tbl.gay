c1fb89aca7c545723398ec1a6a97c1928b37758f
Emily Signet <emily@cat-girl.gay>
2023-08-29 19:24:41 -0400

    cwtp statement update

--- src/routes/statements/cwtp/+page.svelte
+++ src/routes/statements/cwtp/+page.svelte
@@ -3,6 +3,80 @@
 </svelte:head>
 
 <main>
+
+  <h1 style="margin-bottom: 0.2em">
+    <strong>Updated statement on CWTP’s closure</strong>
+  </h1>
+  <p style="text-align:center; margin-top: 0em; margin-bottom:2em;">
+    <em>August 29, 2023</em> <br /><br />
+    <i
+      ><a href="/pdfs/cwtp-update.pdf" target="_blank" rel="noopener noreferrer"
+        >This document is also available in PDF form.</a
+      ></i
+    >
+  </p>
+  <p style="text-align:center; margin-top: 0em; margin-bottom:2em; width: 50%;">
+    This document is an update to a previous statement. For transparency, that statement is available later in this page.
+  </p>
+
+  <p>
+    We apologize again for the haste with which our previous statement was
+    released, and for the inadequacy of our discussion of the CWTP Board's use
+    of security. We have since met at length to discuss this issue; the
+    following is the unanimous statement of the current <em
+      >(as of August 29th, 2023)</em
+    > Collective and Internal Team on that point.
+  </p>
+  <p>
+    On Friday, August 25th, the CWTP Board responded to our inquiry about the
+    use of security as follows: (1) They believed a third party had to be
+    present for safety reasons; (2) They approached multiple non-security
+    resources to request such a person; and (3) They were refused by all of
+    them. This leaves us with two questions.
+  </p>
+  <p>One: Should the Board have proceeded without a third party after all?</p>
+  <p>
+    While we at TBLGAY have successfully navigated dangerous situations alone in
+    the past, sometimes even taking on personal risk to avoid exposing anyone to
+    security, we cannot insist on our fellow students doing this in every
+    imaginable case. However, the kinds of threat which would justify or
+    necessitate the involvement of a historically unpredictable, needlessly
+    militarized, and fundamentally oppressive security force would have to be
+    extreme in the utmost, <em>especially</em> given the marginalized identities
+    of the terminated staff members. We stand firm that this incident was a definite
+    overuse of force in the number of personnel dispatched to attend to the situation.
+  </p>
+  <p>
+    As such, we must and will remain vocally skeptical of this course of action
+    unless and until evidence justifying the use of this force is released for
+    public review. The CWTP Board claims that their reasons are legally
+    confidential, so we do not anticipate such evidence emerging.
+  </p>
+  <p>Two: Should other resources have been made available?</p>
+  <p>
+    To this, the answer is easy: YES. The Board, rightly or wrongly, believed
+    that they needed a third party present. When security turned out to be the
+    only available option, they rightly or wrongly decided to involve them. But
+    it should never have come to this. Similarly, we at TBLGAY should never have
+    had to choose between only the two options of (i) involving security and
+    inviting danger, or (ii) embracing the danger amongst ourselves. That
+    marginalized communities must either risk themselves or ask for intervention
+    from a historically and presently oppressive security force is a shameful
+    state of affairs.
+  </p>
+  <p>
+    We urge the University and the authorities operating within it to urgently
+    address this problem. We will be immediately approaching the York Federation
+    of Students to campaign for new, non-militarized, non-security crisis
+    intervention supports to be made available to the community.
+  </p>
+  <p>
+    Thank you for your patience as we worked on the procedural issues that led
+    to the release of our previous statement — which failed to address several
+    key issues — without proper review and approval. TBLGAY promises to keep our
+    community informed as this rally for change presses forward.
+  </p>
+
   <h1 style="margin-bottom: 0.2em">
     <strong>Statement on CWTP’s closure</strong>
   </h1>
@@ -20,7 +94,11 @@
   <p>
     As you may already know, the Centre for Women and Trans People at York
     University closed its doors on August 18th, 2023. We highly recommend you
-    read <a href="https://drive.google.com/file/d/1sWOeE9xyZ0GLGhWLg9pNoeCGWFNwfeP-/view" target="_blank" rel="noopener noreferrer">the Centre’s official statement on the matter</a> before this one.
+    read <a
+      href="https://drive.google.com/file/d/1sWOeE9xyZ0GLGhWLg9pNoeCGWFNwfeP-/view"
+      target="_blank"
+      rel="noopener noreferrer">the Centre’s official statement on the matter</a
+    > before this one.
   </p>
   <p>
     The news has not been easy on us — both as frequent organizational partners
